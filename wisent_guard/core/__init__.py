2924262b152cd06e8696ee534c25e224044aee75
gracjan.goral <gracjan@wisent.ai>
2025-10-07 00:18:41 +0200

    -- cleaning the code

--- wisent_guard/core/__init__.py
+++ wisent_guard/core/__init__.py
@@ -1,7 +1,6 @@
 # from wisent_guard.core.activations import Activations
 # from wisent_guard.core.classifier.classifier import ActivationClassifier, Classifier
 
-from .secure_code_evaluator import SecureCodeEvaluator, enforce_secure_execution
 from .utils.device import empty_device_cache, preferred_dtype, resolve_default_device, resolve_device, resolve_torch_device
 # from .steering import SteeringMethod, SteeringType
 
@@ -15,11 +14,11 @@ __all__ = [
     # "Model",
     # "ModelParameterOptimizer",
     # "PromptFormat",
-    "SecureCodeEvaluator",
+    # "SecureCodeEvaluator",
     # "SteeringMethod",
     # "SteeringType",
     # "TokenScore",
-    "enforce_secure_execution",
+    # "enforce_secure_execution",
     "empty_device_cache",
     "preferred_dtype",
     "resolve_default_device",
