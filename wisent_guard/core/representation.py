b53fd35a1fe628e480a54a944bd2f4bc79541418
lbartoszcze <lgb2127@columbia.edu>
2025-10-15 20:17:18 +0200

    rename wisent-guard to wisent

--- wisent_guard/core/representation.py
+++
@@ -1,5 +0,0 @@
-class Representation:
-    def __init__(self, tensor, layer, aggregation_method=None):
-        self.tensor = tensor
-        self.layer = layer
-        self.aggregation_method = aggregation_method 
