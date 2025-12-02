bb62a07a9c7999781627553b1af5f1ce6fdba034
bc <bc438397@students.mimuw.edu.pl>
2025-12-02 21:10:53 +0100

    fix few questions

--- wisent/core/contrastive_pairs/lm_eval_pairs/lm_task_extractors/cb.py
+++ wisent/core/contrastive_pairs/lm_eval_pairs/lm_task_extractors/cb.py
@@ -88,7 +88,7 @@ class CBExtractor(LMEvalBenchmarkExtractor):
             correct = labels[label]
             incorrect = labels[(label+1)%3]
         
-            formatted_question = f"{premise}\nQuestion: {hypothesis}. True, False, or Neither?"
+            formatted_question = f"{premise}\nQuestion: {hypothesis}.\nA. {incorrect}\nB. {correct}"
 
             metadata = {
                 "label": "cb",
