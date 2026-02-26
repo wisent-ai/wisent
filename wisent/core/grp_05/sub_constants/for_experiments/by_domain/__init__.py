87ea58222b828640350dbfc1da614804b7eb8dc0
lbartoszcze <lgb2127@columbia.edu>
2026-02-26 15:22:25 -0800

    Replace numbered _exp files with domain-named files
    Restructure ~1550 non-method experiment constants from 8 numbered files
    (_exp_01 through _exp_08 + sub_exp directories) into 7 domain-named files:
    
    by_domain/_optimization.py: OPTUNA_*, SEARCH_*, AUTO_*, SENSITIVITY_*
    by_domain/_evaluation.py: EVAL_*, NLI_*, COHERENCE_*, REFUSAL_*, QUALITY_*
    by_domain/sub_analysis/_geometry.py: GEO_DIAG_*, LINEARITY_*, CONE_*, ZWIAD_*
    by_domain/sub_analysis/_analysis.py: RECOMMEND_*, INTERVENTION_*, SIGNAL_*
    by_domain/sub_analysis/sub_infra/_agent_marketplace.py: AGENT_*, MARKETPLACE_*
    by_domain/sub_analysis/sub_infra/_data_infra.py: PACMAP_*, CLUSTERING_*, EWC_*
    by_domain/sub_analysis/sub_infra/_parser_defaults.py: PARSER_*, MODIFY_WEIGHTS_*
    
    Common steering defaults added to by_method/__init__.py.
    Zero consumer changes - star-import chain preserved.

---
+++ wisent/core/grp_05/sub_constants/for_experiments/by_domain/__init__.py
@@ -0,0 +1,2 @@
+"""Domain-organized experiment constants (optimization, evaluation, geometry, analysis, infra)."""
+from wisent.core.grp_05.sub_constants.for_experiments.by_domain._optimization import *  # noqa: F401,F403
