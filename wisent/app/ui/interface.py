03c1eb97ccb3cb20f5e8f956cc32358b75954cc6
lbartoszcze <lgb2127@columbia.edu>
2026-03-13 14:06:11 -0700

    Revert default model selection back to empty

--- wisent/app/ui/interface.py
+++ wisent/app/ui/interface.py
@@ -253,7 +253,7 @@ def build_interface():
         gr.Dropdown(
             label="Type or select model name",
             choices=list(GRADIO_MODEL_EXAMPLES),
-            value=GRADIO_MODEL_EXAMPLES[INDEX_FIRST],
+            value=None,
             allow_custom_value=True,
             interactive=True,
             elem_id="global-model",
